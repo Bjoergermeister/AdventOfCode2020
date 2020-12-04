652f60c1f4eeb9a3d3e4c8c4b7351c2a42dad56b
Bjarne Svensson <b.svensson@ostfalia.de>
2020-12-04 14:20:12 +0100

    Day 1 Puzzle 1

---
+++ Day1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            HashSet<int> numbers = new HashSet<int>();
+            int goal = 2020;
+
+            foreach(var line in lines)
+            {
+                int number = Convert.ToInt32(line);
+                
+                if (numbers.Contains(number))
+                {
+                    Console.WriteLine(number * (goal - number));
+                    break;
+                }
+                else
+                {
+                    numbers.Add(goal - number);
+                }
+            }
+        }
+    }
+}
