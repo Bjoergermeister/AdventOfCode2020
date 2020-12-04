9193a028c971597e8cff7e14d391a14a9a0bdb45
Bjarne Svensson <b.svensson@ostfalia.de>
2020-12-04 14:59:30 +0100

    Day 1 Puzzle 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day1
 {
@@ -9,24 +10,45 @@ namespace Day1
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
+            int answer = Puzzle2(lines);
+            Console.WriteLine(answer);
+        }
 
-            HashSet<int> numbers = new HashSet<int>();
-            int goal = 2020;
+        static int Puzzle1(string[] input)
+        {
+            var numbers = from line in input select Convert.ToInt32(line);
+            var addingNumbers =  FindAddingNumbers(numbers, 2020);
+            return addingNumbers.Item1 * addingNumbers.Item2;
+        }
 
-            foreach(var line in lines)
+        static int Puzzle2(string[] input)
+        {
+            var numbers = from line in input select Convert.ToInt32(line);
+            
+            foreach(var current in numbers)
             {
-                int number = Convert.ToInt32(line);
-                
-                if (numbers.Contains(number))
-                {
-                    Console.WriteLine(number * (goal - number));
-                    break;
-                }
-                else
-                {
-                    numbers.Add(goal - number);
-                }
+                var addingNumbers = FindAddingNumbers(numbers, 2020 - current, current);
+                if (addingNumbers.Item1 == 0 || addingNumbers.Item2 == 0) continue;
+
+                return current * addingNumbers.Item1 * addingNumbers.Item2;
             }
+
+            return 0;
+        }
+
+        static (int, int) FindAddingNumbers(IEnumerable<int> input, int goal, int excluded = -1)
+        {
+            HashSet<int> numbers = new HashSet<int>();
+
+            foreach(var number in input)
+            {    
+                if (number == excluded) continue;            
+                if (numbers.Contains(number)) return (number, goal - number);
+
+                numbers.Add(goal - number);
+            }
+
+            return (0, 0);
         }
     }
 }
