1a2391f3e0e8bf83ff1937549c0cf6d0f2634f7d
Bjarne Svensson <b.svensson@ostfalia.de>
2020-12-14 00:51:26 +0100

    Day 6 Puzzle 1

---
+++ Day6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int positiveAnswers = 0;
+            int groupStartIndex = 0;
+            int groupEndIndex = 0;
+
+            for(int i = 0; i <= input.Length; i++)
+            {
+                string line = (i < input.Length) ? input[i] : "";
+                if (line.Length == 0)
+                {
+                    string[] copy = new string[groupEndIndex - groupStartIndex];
+                    Array.Copy(input, groupStartIndex, copy, 0, groupEndIndex - groupStartIndex);
+                    positiveAnswers += CountPositiveAnswersInGroup(copy);
+                    groupStartIndex = i + 1;
+                }
+
+                groupEndIndex++;
+            }
+
+            Console.WriteLine(positiveAnswers);
+        }
+
+        static int CountPositiveAnswersInGroup(string[] group)
+        {
+            HashSet<char> positiveAnswers = new HashSet<char>();
+            foreach (string person in group)
+            {
+                foreach (char c in person)
+                {
+                    if (positiveAnswers.Contains(c)) continue;
+                    positiveAnswers.Add(c);
+                }
+            }
+            return positiveAnswers.Count;
+        }
+    }
+}
