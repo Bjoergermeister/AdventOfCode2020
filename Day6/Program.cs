82a6dcc9085a945462b0f573d132c88cf17bf50d
Bjarne Svensson <b.svensson@ostfalia.de>
2020-12-14 01:04:39 +0100

    Day 6 Puzzle 1

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -10,7 +10,8 @@ namespace Day6
         {
             string[] input = File.ReadAllLines("input.txt");
 
-            int positiveAnswers = 0;
+            int positiveAnswersByAnyPerson = 0;
+            int positiveAnswersByEveryPerson = 0;
             int groupStartIndex = 0;
             int groupEndIndex = 0;
 
@@ -21,17 +22,19 @@ namespace Day6
                 {
                     string[] copy = new string[groupEndIndex - groupStartIndex];
                     Array.Copy(input, groupStartIndex, copy, 0, groupEndIndex - groupStartIndex);
-                    positiveAnswers += CountPositiveAnswersInGroup(copy);
+                    positiveAnswersByAnyPerson += CountPositiveAnswersByAnyPerson(copy);
+                    positiveAnswersByEveryPerson += CountPositiveAnswersByEveryPerson(copy);
                     groupStartIndex = i + 1;
                 }
 
                 groupEndIndex++;
             }
 
-            Console.WriteLine(positiveAnswers);
+            Console.WriteLine("Positive Antworten von einigen: " + positiveAnswersByAnyPerson);
+            Console.WriteLine("Positive Antworten von allen: " + positiveAnswersByEveryPerson);
         }
 
-        static int CountPositiveAnswersInGroup(string[] group)
+        static int CountPositiveAnswersByAnyPerson(string[] group)
         {
             HashSet<char> positiveAnswers = new HashSet<char>();
             foreach (string person in group)
@@ -43,6 +46,35 @@ namespace Day6
                 }
             }
             return positiveAnswers.Count;
+        }
+
+        static int CountPositiveAnswersByEveryPerson(string[] group)
+        {
+            Dictionary<char, int> positiveAnswers = new Dictionary<char, int>();
+            foreach(string person in group)
+            {
+                //Count positive answers to each question
+                foreach(char c in person)
+                {
+                    if (positiveAnswers.ContainsKey(c))
+                    {
+                        positiveAnswers[c]++;
+                    }
+                    else
+                    {
+                        positiveAnswers.Add(c, 1);
+                    }
+                }
+            }
+
+            //Check for how many answers the count is as high as the number of members of the group
+            int positiveAnswersByAll = 0;
+            foreach(int answersCount in positiveAnswers.Values)
+            {
+                if (answersCount == group.Length) positiveAnswersByAll++;
+            }
+
+            return positiveAnswersByAll;
         }
     }
 }
