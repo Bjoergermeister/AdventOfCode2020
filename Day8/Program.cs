16ea3706fb8b9349b37158bdb4971ccd239f55be
Bjarne Svensson <b.svensson@ostfalia.de>
2020-12-20 19:30:09 +0100

    Day 8 Puzzle 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -8,26 +8,72 @@ namespace Day8
     {
         static void Main(string[] args)
         {
-            int accumulator = 0;
-            int programmCounter = 0;
-            HashSet<int> executedInstructions = new HashSet<int>();
-
             string[] instructions = File.ReadAllLines("input.txt");
 
-            while (executedInstructions.Contains(programmCounter) == false)
-            {
-                executedInstructions.Add(programmCounter);
+            Console.WriteLine($"Teil 1: {Process(instructions).Item2}");
 
-                string line = instructions[programmCounter];
+            /* ##########
+             * # Part 2 #
+             * ##########
+             */
+
+            //Find all instructions which can be replaced
+            List<int> replaceableInstructions = new List<int>();
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                string instruction = instructions[i].Substring(0, 3);
+                if (instruction == "nop" || instruction == "jmp") replaceableInstructions.Add(i);
+            }
+
+            int currentReplacement = 0;
+            while (replaceableInstructions.Count > 0)
+            {
+                int line = replaceableInstructions[0];
+
+                //Switch
+                instructions[line] = (instructions[line][0] == 'n')
+                    ? instructions[line].Replace("nop", "jmp")
+                    : instructions[line].Replace("jmp", "nop");
+
+                var result = Process(instructions);
+                if (result.Item1)
+                {
+                    Console.WriteLine($"Teil 2: {result.Item2}");
+                    break;
+                }
+                //Switch back
+                instructions[line] = (instructions[line][0] == 'n')
+                    ? instructions[line].Replace("nop", "jmp")
+                    : instructions[line].Replace("jmp", "nop");
+
+                //Increase counter and remove line form list
+                currentReplacement++;
+                replaceableInstructions.RemoveAt(0);
+            }
+
+        }
+
+        static (bool, int) Process(string[] instructions)
+        {
+            HashSet<int> executedInstructions = new HashSet<int>();
+            int accumulator = 0;
+            int programCounter = 0;
+
+            while (executedInstructions.Contains(programCounter) == false)
+            {
+                executedInstructions.Add(programCounter);
+
+                if (programCounter >= instructions.Length) return (true, accumulator);
+                string line = instructions[programCounter];
 
                 string instruction = line.Substring(0, 3);
                 int number = ParseNumber(line.Substring(5, line.Length - 5), line[4]);
 
                 if (instruction == "acc") accumulator += number;
-                programmCounter += (instruction == "jmp") ? number : 1;
+                programCounter += (instruction == "jmp") ? number : 1;
             }
 
-            Console.WriteLine(accumulator);
+            return (false, accumulator);
         }
 
         static int ParseNumber(string input, char prefix)
