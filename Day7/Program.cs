4dc9a86daf4a7dfd742802c022d36bfbedec9050
Bjarne Svensson <b.svensson@ostfalia.de>
2020-12-20 16:45:12 +0100

    Day 7 Puzzle 2

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -7,7 +7,9 @@ namespace Day7
 {
     class Program
     {
-        static Dictionary<string, List<string>> bags = new Dictionary<string, List<string>>();
+        static Dictionary<string, List<Tuple<int, string>>> bags = new Dictionary<string, List<Tuple<int, string>>>();
+        static HashSet<string> countedBags = new HashSet<string>();
+        static Stack<string> bagsToVisit = new Stack<string>();
 
         static void Main(string[] args)
         {
@@ -20,7 +22,7 @@ namespace Day7
                 string containingBagColor = parts[0];
                 if (bags.ContainsKey(containingBagColor) == false)
                 {
-                    bags.Add(containingBagColor, new List<string>());
+                    bags.Add(containingBagColor, new List<Tuple<int, string>>());
                 }
 
                 foreach (string color in parts[1].Split(","))
@@ -28,36 +30,74 @@ namespace Day7
                     if (color == " no other bags.") continue;
                     int length = (color.EndsWith(".")) ? 8 : 7;
                     string containedBagColor = color.Substring(3, color.Length - length).Trim();
+                    int.TryParse(color.Substring(1, 1), out int amout);
 
                     if (bags.ContainsKey(containedBagColor) == false)
                     {
-                        bags.Add(containedBagColor, new List<string>());
+                        bags.Add(containedBagColor, new List<Tuple<int, string>>());
                     }
-                    bags[containedBagColor].Add(containingBagColor);
+                    bags[containingBagColor].Add(Tuple.Create(amout, containedBagColor));
                 }
             }  
 
-            HashSet<string> visitedBags = new HashSet<string>();
-            int foundBags = CountContainingBags("shiny gold", visitedBags);
-            Console.WriteLine(foundBags);
+            int containingCount = CountContainingBags("shiny gold");
+            Console.WriteLine($"Der shiny gold Bag kann von {containingCount} verschiedenen Bags enthalten werden.");
+
+            int containedCount = CountContainedBags("shiny gold");
+            Console.WriteLine($"Der shiny gold Bag enthält {containedCount} Bags.");
         }
 
-        static int CountContainingBags(string color, HashSet<string> visitedBags)
+        static int CountContainingBags(string color)
         {
-            int foundBags = 0;
+            int count = 0;
 
-            var containingBags = bags[color];
-            foreach(string containingBag in containingBags)
+            foreach(var bag in bags)
             {
-                if (visitedBags.Contains(containingBag) == false)
+                string bagColor = bag.Key;
+
+                if (BagCanContain(bagColor, color) == false) continue;
+
+                if (countedBags.Contains(bagColor) == false)
                 {
-                    foundBags++;
-                    visitedBags.Add(containingBag);
+                    countedBags.Add(bagColor);
+                    count++;
                 }
-                foundBags += CountContainingBags(containingBag, visitedBags);
+                bagsToVisit.Push(bagColor);
             }
 
-            return foundBags;
+            while(bagsToVisit.Count > 0)
+            {
+                string nextBagColor = bagsToVisit.Pop();
+                count += CountContainingBags(nextBagColor);
+            }
+
+            return count;
+        }
+
+        static int CountContainedBags(string color)
+        {
+            int count = 0;
+
+            var containedBags = bags[color];
+            if (containedBags.Count == 0) return count;
+
+            foreach(var bag in containedBags)
+            {
+                count += CountContainedBags(bag.Item2) * bag.Item1 + bag.Item1;
+            }
+
+            return count;
+        }
+
+        static bool BagCanContain(string color, string searchedColor)
+        {
+            var containedColors = bags[color];
+            foreach(var containedColor in containedColors)
+            {
+                if (containedColor.Item2 == searchedColor) return true;
+            }
+
+            return false;
         }
     }
 }
