10c2eb76df5aab49fb1222168aa18d878c29741d
Bjarne Svensson <b.svensson@ostfalia.de>
2020-12-26 11:43:36 +0100

    Day 13 Puzzle 1

---
+++ Day13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int earliestDepartureTime = Convert.ToInt32(input[0]);
+
+            var busLines = input[1].Split(",").Where(line => int.TryParse(line, out int busLine)).Select(line => Convert.ToInt32(line));
+
+            int smallestDistance = -1;
+            int bestBusLine = 0;
+            foreach (int line in busLines)
+            {
+                int multiplikator = 1;
+                while (line * multiplikator < earliestDepartureTime)
+                {
+                    multiplikator++;
+                }
+
+                if ((line * multiplikator) - earliestDepartureTime < smallestDistance || smallestDistance == -1)
+                {
+                    smallestDistance = (line * multiplikator) - earliestDepartureTime;
+                    bestBusLine = line;
+                }
+            }
+
+            Console.WriteLine($"Teil 1: {smallestDistance * bestBusLine}");
+        }
+    }
+}
