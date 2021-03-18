f951edf19f853ddac1a94783e3182d9f7d4b9f85
Bjarne Svensson <b.svensson@ostfalia.de>
2021-03-18 08:43:23 +0100

    Day 15 Puzzle 1

---
+++ Day15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> spokenNumbers = new Dictionary<int, int>();
+            int lastSpokenNumber = 0;
+            int turn = 1;
+
+
+            int[] numbers = File.ReadAllText("input.txt").Split(",").Select(number => Convert.ToInt32(number)).ToArray();
+            
+
+            while(turn < 2020)
+            {
+                int number = (turn <= numbers.Length) ? numbers[turn - 1] : lastSpokenNumber;
+
+                if (spokenNumbers.ContainsKey(number))
+                {
+                    lastSpokenNumber = turn - spokenNumbers[number];
+                    spokenNumbers[number] = turn;                    
+                }
+                else
+                {
+                    spokenNumbers.Add(number, turn);
+                    lastSpokenNumber = 0;
+                }
+                turn++;                
+            }
+
+            Console.WriteLine(lastSpokenNumber);     
+        }
+    }
+}
