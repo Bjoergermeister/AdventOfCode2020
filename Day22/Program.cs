78d3646c8b99f18c0619f7e6c936b3b249055152
Bjoergermeister <b.svensson@ostfalia.de>
2021-09-02 00:27:19 +0200

    Day 22 Puzzle 1

---
+++ Day22/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day22
+{
+    class Program
+    {
+        static Queue<int> deck1 = new Queue<int>();
+        static Queue<int> deck2 = new Queue<int>();
+        static void Main(string[] args)
+        {
+            //Process input
+            bool spaceOccured = false;
+            string[] input = File.ReadAllLines("input.txt");
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i].Length == 0)
+                {
+                    spaceOccured = true;
+                    i++;
+                    continue;
+                }
+                int card = Convert.ToInt32(input[i]);
+                if (spaceOccured) deck2.Enqueue(card);
+                else deck1.Enqueue(card);
+            }
+
+            Console.WriteLine("Puzzle 1: " + Puzzle1());
+        }
+
+        static int Puzzle1()
+        {
+            while (deck1.Count > 0 && deck2.Count > 0)
+            {
+                int card1 = deck1.Dequeue();
+                int card2 = deck2.Dequeue();
+
+                if (card1 > card2)
+                {
+                    deck1.Enqueue(card1);
+                    deck1.Enqueue(card2);
+                }
+                else
+                {
+                    deck2.Enqueue(card2);
+                    deck2.Enqueue(card1);
+                }
+            }
+
+            Queue<int> winnersDeck = (deck1.Count > 0) ? deck1 : deck2;
+
+            int result = 0;
+            while (winnersDeck.Count > 0)
+            {
+                result += winnersDeck.Count * winnersDeck.Dequeue();
+            }
+
+            return result;
+        }
+    }
+}
