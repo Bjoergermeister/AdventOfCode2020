f5a6b9196f64de9926bff9b45f0e5fb54c4acada
Bjoergermeister <b.svensson@ostfalia.de>
2021-09-09 14:37:25 +0200

    Day 24 Puzzle 2

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 using System.Text.RegularExpressions;
 
@@ -23,6 +24,7 @@ namespace Day24
             string[] input = File.ReadAllLines("input.txt");
 
             Console.WriteLine("Puzzle 1: " + Puzzle1(input));
+            Console.WriteLine("Puzzle 2: " + Puzzle2());
         }
 
         static int Puzzle1(string[] input)
@@ -51,6 +53,64 @@ namespace Day24
             }
 
             return count;
+        }
+
+        static int Puzzle2()
+        {
+            Dictionary<Vector3, bool> gridCopy = new Dictionary<Vector3, bool>();
+            for (int i = 0; i < 100; i++)
+            {
+                AddMissingSurroundingTiles();
+                CloneDictionary(tiles, gridCopy);
+                foreach (Vector3 position in tiles.Keys.ToList())
+                {
+                    int neighbours = CountNeighbours(gridCopy, position);
+                    if (tiles[position] && (neighbours == 0 || neighbours > 2)) tiles[position] = false;
+                    if (!tiles[position] && neighbours == 2) tiles[position] = true;
+                }
+            }
+
+            int count = 0;
+            foreach (bool isBlack in tiles.Values)
+            {
+                if (isBlack) count++;
+            }
+
+            return count;
+        }
+
+        static void AddMissingSurroundingTiles()
+        {
+            foreach (Vector3 position in tiles.Keys.ToList())
+            {
+                foreach (Vector3 direction in directionVectors.Values)
+                {
+                    if (tiles.ContainsKey(position + direction) == false)
+                    {
+                        tiles.Add(position + direction, false);
+                    }
+                }
+            }
+        }
+
+        static int CountNeighbours(Dictionary<Vector3, bool> grid, Vector3 position)
+        {
+            int count = 0;
+            foreach (Vector3 direction in directionVectors.Values)
+            {
+                if (grid.ContainsKey(position + direction) == false) continue;
+                if (grid[position + direction]) count++;
+            }
+            return count;
+        }
+
+        static void CloneDictionary(Dictionary<Vector3, bool> origin, Dictionary<Vector3, bool> copyTo)
+        {
+            copyTo.Clear();
+            foreach (KeyValuePair<Vector3, bool> entry in origin)
+            {
+                copyTo.Add(entry.Key, entry.Value);
+            }
         }
     }
 }
